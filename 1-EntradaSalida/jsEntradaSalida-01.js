9510e9ef4bbcf7fa96374d8e5f446b1c3ebca673
ezed98 <ezequielnicolasdamore@gmail.com>
2023-01-31 09:46:31 -0300

    modifique ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
-/*
+/* Ezequiel D`Amore
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo!!");
+	alert("Esto funciona de maravilla");
 }
 
